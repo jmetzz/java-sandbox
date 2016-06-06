4ff292162108ad9569971ef10a4074ab523ff218
Jean Metz <jean.metz@ypto.be>
2016-06-06 14:51:50 +0200

    Simple xml processing example with SAX

---
+++ frameworks-lab/src/test/java/com/github/jmetzz/frameworksLab/xml_processing/sax/SaxParsingSchemaWithValidationTest.java
@@ -0,0 +1,24 @@
+package com.github.jmetzz.frameworksLab.xml_processing.sax;
+
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+
+public class SaxParsingSchemaWithValidationTest {
+    public static final String PACKAGE_RESOURCES = "src/test/resources/xml-resources/";
+
+    @Test
+    public void shouldParseOutput() throws Exception {
+        SaxParsingSchemaWithValidation saxParser = new SaxParsingSchemaWithValidation();
+        saxParser.parseOrderXML(PACKAGE_RESOURCES + "order.xml", PACKAGE_RESOURCES + "order.xsd");
+        System.out.println(saxParser.getOutput());
+    }
+
+
+    @Test(expected = SAXException.class)
+    public void shouldRaiseExcelption() throws Exception {
+        SaxParsingSchemaWithValidation saxParser = new SaxParsingSchemaWithValidation();
+        saxParser.parseOrderXML(PACKAGE_RESOURCES + "invalidOrder.xml", PACKAGE_RESOURCES + "order.xsd");
+    }
+
+}
