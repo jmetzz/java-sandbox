0248f2497104d2029a21ccb8f2bc6d83b1406022
Jean Metz <jean.metz@ravago.com>
2017-02-03 17:01:18 +0100

    Centralizes constants definitions

---
+++ design-patterns-lab/src/main/java/com/github/jmetzz/laboratory/design_patterns/Constants.java
@@ -0,0 +1,11 @@
+package com.github.jmetzz.laboratory.design_patterns;
+
+
+public class Constants {
+
+    public static final String BASE_PROJECT_URL = "./design-patterns-lab/";
+    public static final String PACKAGE_RESOURCES = "src/main/resources/";
+    public static final String PACKAGE_TEST_RESOURCES = "src/test/resources/";
+
+
+}
